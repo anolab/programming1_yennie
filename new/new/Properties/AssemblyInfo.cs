c9b0755b84a6bfc9f72ee2882af662ae273599a8
yn.kim <lcherryl333@naver.com>
2017-10-21 17:54:18 +0900

    new

--- new/new/Properties/AssemblyInfo.cs
+++ new/new/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("The Start")]
+[assembly: AssemblyTitle("new")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("The Start")]
+[assembly: AssemblyProduct("new")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3f8d319c-53f5-4e37-b7a2-69ff457e1cdd")]
+[assembly: Guid("c3d5186c-b354-4b9a-9e32-654f672122ec")]
 
 // Version information for an assembly consists of the following four values:
 //
