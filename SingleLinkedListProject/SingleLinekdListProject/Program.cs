fa4edc585d7752727a48547b7f6688e1c01e8f38
yn.kim <lcherryl333@naver.com>
2017-10-21 17:18:23 +0900

    Changes

---
+++ SingleLinkedListProject/SingleLinekdListProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Node
+    {
+        public int info;
+        public Node link;
+
+        public Node(int i) {
+
+            info = i;
+            link = null;
+
+        }    
+
+        }
+    }
+
